ef72c8044f251587689df037677099513753980f
Kiken <modularized@outlook.com>
2024-03-03 04:15:45 +0000

    current progess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+/*
+    PLAN:
+    one struct called FishingGame containing:
+      st: Duration - the starting time, so i can then use it to get an elapsed time later on
+      c_fish: i32 - collected fish, keeps track of how much fish i have
+    
+    methods of FishingGame:
+      count_fish(k: u32) - completely unknown action
+      play() - no fucking clue
+
+    input will be its own function without any methods.
+*/
+
+use std::{io, time::{Instant, Duration}};
+
+struct FishingGame {
+    st: Instant,
+    c_fish: i32,
+}
+
+impl FishingGame {
+    fn count_fish(&mut self) -> bool {
+        if self.c_fish < 100 {
+            print!("GG! ");
+            return true
+        }
+
+        println!("You have collected {}, only {} to go!", self.c_fish, 100 - self.c_fish);
+        false
+    }
+    
+    fn play(&mut self) -> bool {
+        let mut complete: bool = false;
+        while complete == false {
+            self.c_fish = self.c_fish + get_fish_count();
+            complete = self.count_fish();
+        };
+
+        return true;
+    }
+}
+
+fn get_fish_count() -> i32 {
+    let mut input = String::new();
+    
+    println!("How much fish have you collected?: ");
+    io::stdin().read_line(&mut input).expect("failed reading stdin");
+
+    let number: i32 = input.parse::<i32>().expect("couldn't parse the number");
+
+    if number < 0 {
+        panic!("negative number spotted")
+    }
+
+    number
+}
+
+fn format_completion_time(minutes: Duration, seconds: Duration) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = FishingGame {
+        st: Instant::now(),
+        c_fish: get_fish_count(),
+    };
+
+    let mut complete = false;
+    while complete == false {
+        game.play();
+    }
+
+    let et: Duration = game.st.elapsed();
+    println!("That took ")
 }
